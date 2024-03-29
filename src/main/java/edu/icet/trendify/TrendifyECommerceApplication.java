e1d1d71b320cb1e639dc4d75aa9257ee4eaec806
Nadeesha Dassanayake <nadeeshadassanayake96@gmail.com>
2024-03-29 15:46:36 +0530

    Initial commit

---
+++ src/main/java/edu/icet/trendify/TrendifyECommerceApplication.java
@@ -0,0 +1,11 @@
+package edu.icet.trendify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendifyECommerceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TrendifyECommerceApplication.class, args);
+	}
+}
